fd58514ea513734a8ecde0e12d5720f1eed2dd8a
Linhkust <948992793@qq.com>
2023-04-06 20:36:25 +0800

    Finish the data processing

--- gtwr_csm.py
+++ gtwr_csm.py
@@ -1,7 +1,27 @@
-"""
-Selection and Adjustment
+'''
+This package is divided into two components:
 
-"""
+1. SELECTION: select the most similar properties near the target property
+with the consideration of spatial and temporal characteristics.
+
+Parameters determined in this component
+
+1.1 Initial Selection
+Spatial range: distance threshold;
+Temporal range: weeks, months, years, etc.
+
+1.2 Further Selection
+Similarity measure criterion: mutual information based Euclidean distance
+
+After the selection process, we can get the similar properties
+
+2. ADJUSTMENT: when the similar properties are identified, their property value should be
+integrated into the predicted market value of the target property.
+
+How to integrate: a novel weight function using GTWR
+
+'''
+
 
 import pandas as pd
 
@@ -9,13 +29,9 @@ import pandas as pd
 Data Processing
 '''
 
-# Import the data
-data = pd.read_csv('final.csv')
-
-
 def data_processing(data):
     # Delete unwanted column
-    unwanted_columns = ['Source.Name', 'District_html', 'Estate_html', 'Unit Address', 'Contract', 'Cat_ID', 'Arearaw',
+    unwanted_columns = ['Source.Name', 'District_html', 'Estate_html', 'Contract', 'Cat_ID', 'Arearaw',
                         'Deal_ID']
     df = data.drop(unwanted_columns, axis=1)
 
@@ -24,15 +40,31 @@ def data_processing(data):
 
     # Outliers, missing values, inconsistent values
 
-    # Outliers in price
+    # Outliers in price: different units of price (HK$ and million HK$)
     sorted_price = data['Price'].sort_values()  # sort the values as ascending order
-
     diff = sorted_price.diff()  # calculate the difference
-
     gap_point = sorted_price.loc[diff.idxmax()]  # find the data gap point
-
     df['Price'] = df['Price'].apply(lambda x: x / 1e6 if x >= gap_point else x)  # HK$ to Million HK$
+
+    # Inconsistent values: Delete the unit of GFA and SFA
+    df['GFA'] = df['GFA'].apply(lambda x: x.replace('ft²', ''))
+    df['SFA'] = df['SFA'].apply(lambda x: x.replace('ft²', ''))
+
+    # delete missing values of GFA and SFA
+    df = df.drop(df[(df['GFA'] == '--') & (df['SFA'] == '--')].index)
+    df['Area'] = df.apply(lambda x: x.GFA if x.GFA != '--' else x.SFA, axis=1)
+
+    df = df.drop(['GFA', 'SFA', 'Change', 'GFA Price', 'SFA Price'], axis=1)
+
+    df.to_csv('data.csv', index=False, encoding='utf_8_sig')
+
+
+# Feature construction
+def spatial_information(data, index):
+    address = data.loc[index, 'Estate'] + ' ' + data.loc[index, 'Block']
+    print(address)
 
 
 if __name__ == "__main__":
+    data = pd.read_csv('final.csv')
     data_processing(data)
