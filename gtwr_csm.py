a9c8b70293cdc4a31ca8ade0c20eb47c75bd02cb
Linhkust <948992793@qq.com>
2023-05-03 18:42:16 +0800

    Initial spatial and temporal selection of properties

--- gtwr_csm.py
+++ gtwr_csm.py
@@ -21,6 +21,7 @@ integrated into the predicted market value of the target property.
 How to integrate: a novel weight function using GTWR
 
 '''
+import datetime
 import time
 import pandas as pd
 import requests
@@ -32,11 +33,14 @@ from scipy.stats import entropy
 import concurrent.futures
 from functools import partial
 import warnings
+from datetime import datetime, timedelta
+
 
 warnings.filterwarnings('ignore')
 
 '''
 Data Processing
+Output: final data
 '''
 
 
@@ -99,7 +103,7 @@ def spatial_information(data):
     data.to_csv('data_xy.csv', index=None)
 
 
-def facility_variables(row, data, facility, wifi, threshold):
+def facility_variables(row, data, facility, wifi):
     # HKGrid1980 to WGS84
     tf = Transformer.from_crs('epsg:2326', 'epsg:4326', always_xy=True)
 
@@ -181,16 +185,14 @@ def facility_variables(row, data, facility, wifi, threshold):
     for j in range(len(facility_type)):
         distance = facilities_1km[facilities_1km['TYPE'] == facility_type[j]]['distance'].min()
         variables[facility_type[j]] = distance
-        variables[facility_type[j] + '_walk'] = 1 if distance < threshold else 0
-
     return variables
 
 
-def parallel_computing(worker, data, facility, wifi, threshold):
+def parallel_computing(worker, data, facility, wifi):
     # Assign the core missions
 
     iter_num = len(data) // worker
-    fun_partial = partial(facility_variables, data=data, facility=facility, wifi=wifi, threshold=threshold)
+    fun_partial = partial(facility_variables, data=data, facility=facility, wifi=wifi)
 
     info = []
     for i in tqdm(range(iter_num+1)):
@@ -212,18 +214,92 @@ def parallel_computing(worker, data, facility, wifi, threshold):
 
     df = pd.concat([pd.DataFrame(l, index=[0]) for l in info], axis=0, ignore_index=True)
     df = pd.concat([data, df], axis=1)
-    df.to_csv('spatial_variables.csv', index=False)
+    df.to_csv('data_variables.csv', index=False)
+
+
+'''
+Variable Creation and Algorithm Design
+'''
+
+
+def gtwr_knn(data, threshold, spatial, temporal):
+    # variable creation: select the independent and dependent variables
+    # This is mainly about the POI related variables
+
+    # POIs that significantly affect the housing prices
+    selected_poi = ['MAL', 'SMK', 'KDG', 'PRS', 'SES', 'PAR', 'PLG', 'RGD', 'BUS', 'MIN', 'CPO', 'MTA']
+    for i, poi in enumerate(selected_poi):
+        data[poi+'_Walk'] = data.apply(lambda x: 1 if x[poi] <= threshold else 0, axis=1)
+
+    # define the x and y variables
+    x_variables = ['Date',
+                   'Floor',
+                   'Area',
+                   'x',
+                   'y',
+                   'wifi_hk',
+                   'POI_density',
+                   'Num_class',
+                   'Num_type',
+                   'Class_diversity',
+                   'Type_diversity'] + [j+'_Walk' for j in selected_poi]
+    y_variable = ['Price']
+
+    x = data[x_variables]
+    y = data[y_variable]
+
+    # Initial selection
+    for i in range(len(data)):
+        # Use drop function to obtain all properties except the target one
+        other_properties = data.drop(i)
+
+        # Spatial_proximity
+        rectangle_properties = other_properties[(other_properties['x'] <= data.loc[i, 'x'] + spatial) &
+                                                (other_properties['x'] >= data.loc[i, 'x'] - spatial)&
+                                                (other_properties['y'] <= data.loc[i, 'y'] + spatial)&
+                                                (other_properties['y'] >= data.loc[i, 'y'] - spatial)]
+
+        rectangle_properties['distance'] = rectangle_properties.apply(lambda x: geodesic((data.loc[i, 'Latitude'],
+                                                                      data.loc[i, 'Longitude']),
+                                                                      (x['Latitude'],
+                                                                      x['Longitude'])).m,
+                                                                      axis=1)
+
+        spatial_properties = rectangle_properties[rectangle_properties['distance'] <= spatial]
+
+        # Temporal_proximity
+        spatial_properties['Date'] = pd.to_datetime(spatial_properties['Date'], format='%d/%m/%Y')
+
+        # Temporal gap
+        delta = timedelta(days=temporal)
+
+        transaction_date = datetime.strptime(data.loc[i, 'Date'], '%d/%m/%Y')
+        start_time = transaction_date - delta
+
+        # Select the properties within the selected time period
+        spatial_temporal_properties = spatial_properties[(spatial_properties['Date'] >= start_time) &
+                                                         (spatial_properties['Date'] <= transaction_date)].reset_index(drop=True)
+        spatial_temporal_properties['time'] = spatial_temporal_properties.apply(lambda x: (transaction_date-x['Date']).total_seconds()/86400, axis=1)
+        print(spatial_temporal_properties)
+        break
 
 
 if __name__ == "__main__":
     # data import
-    property_data = pd.read_csv('data_xy.csv', encoding='unicode_escape')
-    facility_data = pd.read_csv('GeoCom4.0_202203.csv', low_memory=False)
-    wifi_data = pd.read_csv('WIFI_HK.csv', low_memory=False)
-    walk_threshold = 400
-    processor = 8
+    # property_data = pd.read_csv('data_xy.csv', encoding='unicode_escape')
+    # facility_data = pd.read_csv('GeoCom4.0_202203.csv', low_memory=False)
+    # wifi_data = pd.read_csv('WIFI_HK.csv', low_memory=False)
 
-    parallel_computing(processor, property_data, facility_data, wifi_data, walk_threshold)
+    # processor = 8
+    #
+    # parallel_computing(processor, property_data, facility_data, wifi_data)
+
+    # GTWR-based algorithm design
+    walk_threshold = 400
+    spatial_bandwidth = 500
+    temporal_bandwidth = 30
+    data = pd.read_csv('data_variables.csv', encoding='unicode_escape')
+    gtwr_knn(data, walk_threshold, spatial_bandwidth, temporal_bandwidth)
 
 
 
